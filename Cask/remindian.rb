f6c67dd42d66e757225fd9805522038e4f330fd1
David Phelan <816556+dphelan@users.noreply.github.com>
2026-07-16 17:02:42 -0400

    Fix DMG download path & inaccurate description for Homebrew Cask

--- Cask/remindian.rb
+++ Cask/remindian.rb
@@ -2,9 +2,9 @@ cask "remindian" do
   version "5.25.13"
   sha256 "3a3fbdf98fb3ce812a2cfc97c837c77e438dc74fa2fcdeef7e58f6a53d0fd724"
 
-  url "https://github.com/Santofer/Remindian/releases/download/v#{version}/Remindian-v#{version}.dmg"
+  url "https://github.com/Santofer/Remindian/releases/download/v#{version}/Remindian-#{version}.dmg"
   name "Remindian"
-  desc "Sync Obsidian tasks with Apple Reminders, Todoist, Asana, Linear, and more"
+  desc "Sync Obsidian tasks with Apple Reminders, Things 3, Todoist, and TickTick"
   homepage "https://github.com/Santofer/Remindian"
 
   auto_updates true
